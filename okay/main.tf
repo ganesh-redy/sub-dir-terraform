63247bebac4683c1e67d469615d4f4ad27a991de
ganesh-redy <ganeshreddy7610@gmail.com>
2025-02-11 18:16:28 +0530

    Create main.tf

---
+++ okay/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+    project = "mythic-inn-420620"
+    zone="us-cetral1"
+  
+  
+}
+
+resource "google_compute_instance" "n1" {
+    name="n1"
+    machine_type = "e2-micro"
+    boot_disk {
+      initialize_params {
+        image = "centos-stream-9"
+      }
+    }
+    network_interface {
+      network = "default"
+      access_config {
+        
+      }
+    }
+  
+}
